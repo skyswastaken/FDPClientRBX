e5820b373d729ac896e78d85af66014a86fb1778
skys <98858283+skyswastaken@users.noreply.github.com>
2022-07-25 15:46:27 -0600

    Create bedwars.lua

---
+++ main/Custom modules/bedwars.lua
@@ -0,0 +1,3 @@
+local balloonpop = 
+game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.mvlfdh:FireServer()
+local ballonuse = game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.adcgddaEuv:FireServer()
