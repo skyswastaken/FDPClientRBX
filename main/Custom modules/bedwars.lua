bc513865f92477a56c8481ee3b3e415913d281e2
skys <98858283+skyswastaken@users.noreply.github.com>
2022-07-28 16:39:15 -0600

    Update bedwars.lua
    added the script fr

--- main/Custom modules/bedwars.lua
+++ main/Custom modules/bedwars.lua
@@ -1,3 +1,1 @@
-local balloonpop = 
-game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.mvlfdh:FireServer()
-local ballonuse = game:GetService("ReplicatedStorage").rbxts_include.node_modules.net.out._NetManaged.adcgddaEuv:FireServer()
+
